16d5363cf63dfcf57715d55c585b50664afd3b93
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-06-24 20:50:07 +0100

    Tp donne 100%

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,38 +6,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-    <h1>All Stagiaires</h1>
+    <div class="container mt-5">
+        <h1 class="mb-4">All Stagiaires</h1>
+        <a href="{{ route('Stagiaires.create') }}" class="btn btn-primary mb-3">Add New Stagiaire</a>
 
-    <table>
-        <thead>
-            <th>Nom </th>
-            <th>Prenom</th>
-            <th>filiere</th>
-            <th>Action</th>
-        </thead>
-        <tbody>
-            @foreach ($Stagiaires as $Stagiaire)
+        <table class="table table-striped">
+            <thead class="thead-dark">
                 <tr>
-                    <td>{{ $Stagiaire->nom }}</td>
-                    <td>{{ $Stagiaire->prenom }}</td>
-                    <td>{{ $Stagiaire->filiere }}</td>
-                    <td>
-                        <form action="{{ route('Stagiaires.destroy', $Stagiaire->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit">Delete</button>
-                        </form>
-                        <button><a href="{{route("Stagiaires.edit",$Stagiaire->id)}}">Edit</a></button>
-                    </td>
+                    <th>Nom</th>
+                    <th>Prenom</th>
+                    <th>Filiere</th>
+                    <th>Action</th>
                 </tr>
-            @endforeach
+            </thead>
+            <tbody>
+                @foreach ($Stagiaires as $Stagiaire)
+                    <tr>
+                        <td>{{ $Stagiaire->nom }}</td>
+                        <td>{{ $Stagiaire->prenom }}</td>
+                        <td>{{ $Stagiaire->filiere }}</td>
+                        <td>
+                            <form action="{{ route('Stagiaires.destroy', $Stagiaire->id) }}" method="POST" style="display: inline;">
+                                @csrf
+                                @method('DELETE')
+                                
+                                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                            </form>
+                            <a href="{{ route('Stagiaires.edit', $Stagiaire->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
-
-        </tbody>
-    </table>
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
